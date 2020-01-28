bfb5b74d36817f6fa0a28a9ccd028ab41454bc17
ecyas <ecyas@yandex.ru>
2020-01-29 00:46:10 +0300

    my first commit

---
+++ Variable.java
@@ -0,0 +1,21 @@
+public class Variable {
+  public static void main(String[] args) {
+    byte numOfCores = 6;
+    short displaySize = 15;
+    int frequency = 2200;
+    long hddSizeInMb = 250000L;
+    float someVar = 675.67f;
+    double cost = 2000.4;
+    char label = 'M';
+    boolean isWork = true;
+
+    System.out.println(numOfCores);
+    System.out.println(displaySize);
+    System.out.println(frequency);
+    System.out.println(hddSizeInMb);
+    System.out.println(someVar);
+    System.out.println(cost);
+    System.out.println(label);
+    System.out.println(isWork);
+  }
+}
